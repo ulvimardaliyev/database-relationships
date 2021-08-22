6bd7cfd70cf60c3640b27eedf90cc068f552eefe
Samir <samir.aghjayev@gmail.com>
2021-08-22 09:54:48 +0400

    Initial Commit

---
+++ src/main/java/az/crocusoft/database/releationships/DatabaseReleationshipsApplication.java
@@ -0,0 +1,13 @@
+package az.crocusoft.database.releationships;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseReleationshipsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatabaseReleationshipsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/az/crocusoft/database/releationships/DatabaseReleationshipsApplicationTests.java
@@ -0,0 +1,13 @@
+package az.crocusoft.database.releationships;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseReleationshipsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
